870f19ce40ab3a33306661e66617280a5f942961
John Cordier <jcordier002@student.francsican.edu>
2024-12-08 18:56:48 -0500

    Not_important_changes

--- equationsolver.py
+++ equationsolver.py
@@ -9,4 +9,57 @@ def equation_solver():
         print(solutions)
     else:
         print("No solutions.")
-equation_solver()
+
+# str = "1,2,3,4,5,"
+# g = 0
+# for l in range(str.count(",")):
+#     y = str.index(",",g)
+#     z = str[g:y]
+#     print(z)
+#     g=y+1
+
+
+# def enter():
+#     try:
+#         if "{" in answer["text"]:
+#             answer["text"]=answer["text"].replace("{", ",")
+#             answer["text"]=answer["text"].replace("}", ",")
+#         if "^" in expr["text"]:
+#             expr["text"]=expr["text"].replace("^", "**")
+#         if "√" in expr["text"]:
+#             expr["text"]=expr["text"].replace("√", "sqrt")
+#         if ("e") in expr["text"]:
+#             expr["text"]=expr["text"].replace("e", "E")
+#         if "π" in expr["text"]:
+#             expr["text"]=expr["text"].replace("π","pi")
+#         save = expr["text"]
+#         if ("x" in save) and ("x" not in answer["text"]):
+#             if "," in answer:
+#                 r=[]
+#                 g = 0
+#                 for l in range(answer["text"].count(",")):
+#                     y = answer["text"].index(",",g)
+#                     eqchunk = str[g:y]
+#                     z = sp.symbols('z')
+#                     d = sp.sympify(eqchunk)
+#                     func = sp.lambdify(z,d)
+#                     r.append(str(func(1)))
+#                     g=y+1
+#                 answer["text"] = str(r)
+#             else:
+#                 z = sp.symbols('z')
+#                 y = sp.sympify(answer["text"])
+#                 func = sp.lambdify(z,y)
+#                 answer["text"] = str(func(1))
+#         elif ("x" in save) and ("x" in answer):
+#             answer["text"] = "Can't Evaluate"
+#         else:
+#             z = sp.symbols('z')
+#             y = sp.sympify(save)
+#             func = sp.lambdify(z,y)
+#             answer["text"] = str(func(1))
+#             expr["text"] = ""
+#             history = hist(save,answer["text"])
+#             history.pack()
+#     except Exception as e:
+#         answer["text"] = "Error"
